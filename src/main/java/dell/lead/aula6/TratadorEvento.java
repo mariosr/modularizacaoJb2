dcf43009b7262aa3ff81ce67e737648d1a79c092
mario falcao <mariofalcao.es@gmail.com>
2017-05-25 22:11:47 -0300

    usando o override

--- src/main/java/dell/lead/aula6/TratadorEvento.java
+++ src/main/java/dell/lead/aula6/TratadorEvento.java
@@ -1,19 +1,15 @@
 package dell.lead.aula6;
 
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 
-public class TratadorEvento implements EventHandler { // 1
-
-	public void handle(ActionEvent evento) { // 2
-		System.out.println("Evento tratado por uma classe externa");
-	}
+@SuppressWarnings("restriction")
+public class TratadorEvento implements EventHandler {
 
 	@Override
 	public void handle(Event arg0) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Evento tratado por uma classe externa");
+
 	}
 
 }
